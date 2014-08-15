5c87456c024a6c0a56820b6d2f52c12bc77516ff
Roger Hoover <roger.hoover@gmail.com>
2014-08-14 18:42:29 -0700

    Basic args parsing

--- tools/src/main/java/com/quantiply/firestone/tools/KafkaAvroLoader.java
+++ tools/src/main/java/com/quantiply/firestone/tools/KafkaAvroLoader.java
@@ -1,9 +1,29 @@
 package com.quantiply.firestone.tools;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 public class KafkaAvroLoader {
   
   public static void main(String[] args) {
-	  System.out.println("Hello");
+    ArgumentParser parser = ArgumentParsers
+            .newArgumentParser("KafkaAvroLoader")
+            .defaultHelp(true)
+            .description("Load Avro file to Kafka");
+    parser.addArgument("-b", "--bucket").required(true)
+            .help("bucket");
+    parser.addArgument("-s", "--stream").required(true)
+        .help("stream");
+    Namespace ns = null;
+    try {
+        ns = parser.parseArgs(args);
+    }
+    catch (ArgumentParserException e) {
+        parser.handleError(e);
+        System.exit(1);
+    }
   }
 
 }
