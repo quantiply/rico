e5130f718cd9b855f0f6371aebc58999de5b241a
Roger Hoover <roger.hoover@gmail.com>
2014-08-24 15:36:23 -0700

    JSON loader

--- tools/src/main/java/com/quantiply/rico/tools/KafkaJSONLoader.java
+++ tools/src/main/java/com/quantiply/rico/tools/KafkaJSONLoader.java
@@ -1,6 +1,22 @@
 package com.quantiply.rico.tools;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.UUID;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.log4j.BasicConfigurator;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.quantiply.rico.common.codec.Headers;
+import com.quantiply.rico.common.codec.StringEncoder;
+import com.quantiply.rico.common.codec.StringMessage;
 
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
@@ -9,7 +25,7 @@ import net.sourceforge.argparse4j.inf.Namespace;
 
 public class KafkaJSONLoader {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BasicConfigurator.configure();
         Namespace ns = parseCmdLineArgs(args);
 
@@ -17,11 +33,43 @@ public class KafkaJSONLoader {
         String bucket = ns.getString("bucket");
         String stream = ns.getString("stream");
         String fileName = ns.getString("file");
+        String timeField = ns.getString("timeField");
+        String keyField = ns.getString("keyField");
         
         String topic = String.format("pub.%s.%s.%s", tenant, bucket, stream);
         
         System.out.println(ns.toString());
-
+        
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        KafkaProducer producer = new KafkaProducer(props);
+        
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                ProducerRecord record = getProducerRecord(topic, line, timeField, keyField);
+                producer.send(record);
+            }
+        }
+        finally {
+            br.close();
+            producer.close();
+        }
+    }
+    
+    private static ProducerRecord getProducerRecord(String topic, String jsonRec, String timeField, String keyField) throws IOException {
+        String id = UUID.randomUUID().toString();
+        JsonObject jObj = new JsonParser().parse(jsonRec).getAsJsonObject();
+        DateTime occurred = ISODateTimeFormat.dateTime().parseDateTime(jObj.get(timeField).getAsString());
+        Headers hdrs = new Headers(id, occurred);
+        StringMessage msg = new StringMessage(jsonRec, hdrs);
+        StringEncoder encoder = new StringEncoder();
+        byte[] key = null;
+        if (keyField != null) {
+            key = jObj.get(keyField).getAsString().getBytes(java.nio.charset.StandardCharsets.UTF_8);
+        }
+        return new ProducerRecord(topic, key, encoder.encode(msg));
     }
 
     private static Namespace parseCmdLineArgs(String[] args) {
@@ -35,7 +83,7 @@ public class KafkaJSONLoader {
             .help("bucket");
         parser.addArgument("-s", "--stream").required(true)
             .help("stream");
-        parser.addArgument("--timestamp").required(false).setDefault("@timestamp")
+        parser.addArgument("--timeField").required(false).setDefault("@timestamp")
             .help("timestamp field (ISO 1806 format)");
         parser.addArgument("-k", "--keyField").required(false)
             .help("partition key field");
