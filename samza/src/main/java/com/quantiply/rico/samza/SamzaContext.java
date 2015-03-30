be54b2d196ef5d6c38fbeb7a73b951a35bdcc7e3
Roger Hoover <roger.hoover@gmail.com>
2015-03-30 00:10:45 -0700

    Refactored metrics to use coda-metrics package

--- samza/src/main/java/com/quantiply/rico/samza/SamzaContext.java
+++ samza/src/main/java/com/quantiply/rico/samza/SamzaContext.java
@@ -3,17 +3,18 @@ package com.quantiply.rico.samza;
 import com.codahale.metrics.*;
 import com.quantiply.rico.Context;
 import com.quantiply.rico.KeyValueStore;
+import com.quantiply.rico.Metrics;
 import org.apache.samza.task.TaskContext;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class SamzaContext implements Context{
+public class SamzaContext implements Context {
 
-    private TaskContext _internal;
-    private Map<String, KeyValueStore<String, Object>> stores  = new HashMap<>();
+    private final TaskContext _internal;
+    private final Map<String, KeyValueStore<String, Object>> _stores = new HashMap<>();
+    private final Map<String, Metrics> _metrics = new HashMap<>();
     static final MetricRegistry _registry = new MetricRegistry();
-
 
     public SamzaContext(TaskContext ctx) {
         this._internal = ctx;
@@ -21,45 +22,21 @@ public class SamzaContext implements Context{
 
     @Override
     public KeyValueStore getKeyValueStore(String name) {
-        if(!stores.containsKey(name)) {
+        if(!_stores.containsKey(name)) {
             // This is ugly. Think of a better name for K,V store for Rico API.
             org.apache.samza.storage.kv.KeyValueStore<String, Object> s
                     = (org.apache.samza.storage.kv.KeyValueStore<String, Object>) _internal.getStore(name);
-            stores.put(name, new SamzaKeyValueStore(s));
+            _stores.put(name, new SamzaKeyValueStore(s));
         }
-        return stores.get(name);
+        return _stores.get(name);
     }
 
     @Override
-    public Histogram histogram(String name) {
-        Histogram h = _registry.histogram(name);
-        _internal.getMetricsRegistry().newGauge(name, new MapGauge(name, h));
-        return h;
+    public Metrics metrics(String group) {
+        if(!_metrics.containsKey(group)) {
+            _metrics.put(group, new SamzaMetrics(_registry, _internal.getMetricsRegistry(), group));
+        }
+        return _metrics.get(group);
     }
 
-    @Override
-    public Counter counter(String name) {
-        Counter h = _registry.counter(name);
-        _internal.getMetricsRegistry().newGauge(name, new MapGauge(name, h));
-        return h;
-    }
-
-    @Override
-    public Timer timer(String name) {
-        Timer h = _registry.timer(name);
-        _internal.getMetricsRegistry().newGauge(name, new MapGauge(name, h));
-        return h;
-    }
-
-    @Override
-    public Meter meter(String name) {
-        Meter h = _registry.meter(name);
-        _internal.getMetricsRegistry().newGauge(name, new MapGauge(name, h));
-        return h;
-    }
-
-    @Override
-    public void gauge(String name, Gauge g) {
-        _internal.getMetricsRegistry().newGauge(name, new MapGauge(name, g));
-    }
 }
