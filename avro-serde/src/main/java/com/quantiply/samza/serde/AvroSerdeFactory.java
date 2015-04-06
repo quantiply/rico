22361f9423e221fe636e4968a4987879460e3bfa
Roger Hoover <roger.hoover@gmail.com>
2015-04-06 09:08:45 -0700

    Added option for specific reader

--- avro-serde/src/main/java/com/quantiply/samza/serde/AvroSerdeFactory.java
+++ avro-serde/src/main/java/com/quantiply/samza/serde/AvroSerdeFactory.java
@@ -10,11 +10,12 @@ public class AvroSerdeFactory implements SerdeFactory<Object> {
     @Override
     public AvroSerde getSerde(String s, Config config) {
         final String registryUrl = config.get("confluent.schema.registry.url");
+        final String specificReader = config.get("confluent.specific.avro.reader", "true");
         final Properties encoderProps = new Properties();
         encoderProps.setProperty("schema.registry.url", registryUrl);
         final Properties decoderProps = new Properties();
         decoderProps.setProperty("schema.registry.url", registryUrl);
-        decoderProps.setProperty("specific.avro.reader", "true");
+        decoderProps.setProperty("specific.avro.reader", specificReader);
         return new AvroSerde(new VerifiableProperties(encoderProps), new VerifiableProperties(decoderProps));
     }
 }
