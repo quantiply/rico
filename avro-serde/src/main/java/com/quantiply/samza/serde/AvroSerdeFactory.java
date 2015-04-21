4a5aad22467a43dd1249cb1b56b5a28790239134
Roger Hoover <roger.hoover@gmail.com>
2015-04-20 22:52:20 -0700

    Checking for schema registry property

--- avro-serde/src/main/java/com/quantiply/samza/serde/AvroSerdeFactory.java
+++ avro-serde/src/main/java/com/quantiply/samza/serde/AvroSerdeFactory.java
@@ -1,5 +1,6 @@
 package com.quantiply.samza.serde;
 
+import com.quantiply.rico.errors.ConfigException;
 import kafka.utils.VerifiableProperties;
 import org.apache.samza.config.Config;
 import org.apache.samza.serializers.SerdeFactory;
@@ -7,9 +8,14 @@ import org.apache.samza.serializers.SerdeFactory;
 import java.util.Properties;
 
 public class AvroSerdeFactory implements SerdeFactory<Object> {
+    public static String CFG_SCHEMA_REGISTRY_URL = "confluent.schema.registry.url";
+
     @Override
     public AvroSerde getSerde(String s, Config config) {
-        final String registryUrl = config.get("confluent.schema.registry.url");
+        final String registryUrl = config.get(CFG_SCHEMA_REGISTRY_URL);
+        if (registryUrl == null) {
+            throw new ConfigException("Missing property: " + CFG_SCHEMA_REGISTRY_URL);
+        }
         final String specificReader = config.get("confluent.specific.avro.reader", "true");
         final Properties encoderProps = new Properties();
         encoderProps.setProperty("schema.registry.url", registryUrl);
