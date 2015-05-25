48cb467cb3e9a25427424a1e2c6b79c0405cc593
Roger Hoover <roger.hoover@gmail.com>
2015-05-25 16:34:34 -0700

    Added readme

---
+++ integration/README.md
@@ -0,0 +1,6 @@
+To run the tests:
+
+```
+pip install behave
+behave
+```
--- integration/core/environment.py
+++ integration/core/environment.py
@@ -2,5 +2,5 @@ import subprocess
 
 def before_all(context):
     build_cmd = "mvn -f cmd-line-pom.xml -DbuildDir=build clean package"
-    #subprocess.check_call(build_cmd, shell=True)
+    subprocess.check_call(build_cmd, shell=True)
     
