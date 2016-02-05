31ca9df464cb9c4f6eb8766903406b5c36a2429b
Roger Hoover <roger.hoover@gmail.com>
2016-02-04 17:31:45 -0800

    Unit test for max interval flush

--- samza-elasticsearch/src/test/java/com/quantiply/elasticsearch/HTTPBulkLoaderTest.java
+++ samza-elasticsearch/src/test/java/com/quantiply/elasticsearch/HTTPBulkLoaderTest.java
@@ -10,10 +10,19 @@ import io.searchbox.core.DocumentResult;
 import io.searchbox.params.Parameters;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.security.InvalidParameterException;
 import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 
+import static com.jayway.awaitility.Awaitility.await;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class HTTPBulkLoaderTest {
 
@@ -82,10 +91,79 @@ public class HTTPBulkLoaderTest {
     assertEquals(null, action.getData(new Gson()));
   }
 
+  @Test
+  public void testDeadWriterDetectionOnAdd() throws Throwable {
+    HTTPBulkLoader loader = getBulkLoader();
+    loader.writerFuture = mock(Future.class);
+    when(loader.writerFuture.isDone()).thenReturn(true);
+    when(loader.writerFuture.get()).thenThrow(new RuntimeException("TEST"));
+
+    HTTPBulkLoader.ActionRequest req = getRequest();
+    loader.addAction("test", req); //this should not fail b/c queue is not yet full
+
+    //This should fail b/c queue is full
+    assertThatThrownBy(() -> loader.addAction("test", req)).isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("TEST");
+  }
+
+  @Test
+  public void testDeadWriterDetectionOnFlushCmdSend() throws Throwable {
+    HTTPBulkLoader loader = getBulkLoader();
+    loader.writerFuture = mock(Future.class);
+    when(loader.writerFuture.isDone()).thenReturn(true);
+    when(loader.writerFuture.get()).thenThrow(new RuntimeException("TEST"));
+
+    HTTPBulkLoader.ActionRequest req = getRequest();
+    loader.addAction("test", req); //this should not fail b/c queue is not yet full
+
+    //This should fail b/c queue is full
+    assertThatThrownBy(loader::flush).isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("TEST");
+  }
+
+  @Test
+  public void testDeadWriterDetectionOnFlushWait() throws Throwable {
+    HTTPBulkLoader loader = getBulkLoader();
+    loader.writerFuture = mock(Future.class);
+    when(loader.writerFuture.isDone()).thenReturn(true);
+    when(loader.writerFuture.get()).thenThrow(new RuntimeException("TEST"));
+
+    //This on wait timeout - not because queue is full
+    assertThatThrownBy(loader::flush).isInstanceOf(RuntimeException.class)
+            .hasMessageContaining("TEST");
+  }
+
+  @Test
+  public void testWriterMaxInterval() throws Throwable {
+    int maxIntervalMs = 150;
+    HTTPBulkLoader.Config config = new HTTPBulkLoader.Config("Test", 1, Optional.of(maxIntervalMs));
+    JestClient client = mock(JestClient.class);
+    AtomicInteger numFlushes = new AtomicInteger(0);
+    HTTPBulkLoader loader = new HTTPBulkLoader(config, client, Optional.of(bulkReport -> numFlushes.incrementAndGet()));
+
+    loader.start();
+    Thread.sleep(maxIntervalMs + 10);
+    //Will not flush on max interval if no docs were added
+    assertEquals(0, numFlushes.get());
+    loader.addAction("test", getRequest());
+    //Not that there's a record it should have flushed
+    await().atMost(maxIntervalMs, TimeUnit.MILLISECONDS).until(() -> numFlushes.get() == 1);
+    loader.stop();
+  }
+
+  private HTTPBulkLoader.ActionRequest getRequest() {
+    long tsNow = 12345L;
+    ActionRequestKey key = ActionRequestKey.newBuilder()
+            .setAction(Action.INDEX)
+            .setId("blah")
+            .build();
+    return new HTTPBulkLoader.ActionRequest(key, "testindex", "testtype", tsNow, "{}");
+  }
 
   private HTTPBulkLoader getBulkLoader() {
-    HTTPBulkLoader.Config config = new HTTPBulkLoader.Config("Test", 20, Optional.empty());
+    HTTPBulkLoader.Config config = new HTTPBulkLoader.Config("Test", 1, Optional.empty());
     JestClient client = mock(JestClient.class);
     return new HTTPBulkLoader(config, client, Optional.empty());
   }
+
 }
